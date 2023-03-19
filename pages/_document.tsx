6189da12baad21db92acafeef2db289634c08b45
scottjason <scottleejason@gmail.com>
2023-03-19 16:09:46 -0700

    fix next/image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,10 @@ export default function Document() {
       <body className='w-screen h-screen flex items-center justify-center flex-col bg-black-900'>
         <Image
           alt='next logo img'
-          className='absolute top-5 left-5 w-[80px]'
-          src={'./nextjs-white.webp'}
+          width={80}
+          height={80}
+          className='absolute top-5 left-5'
+          src={'/nextjs-white.webp'}
         />
         <Main />
         <NextScript />
