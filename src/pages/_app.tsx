e170f9b56f66c718c02986cae48ab94cf8572ed5
scottjason <scottleejason@gmail.com>
2023-03-25 08:54:54 -0700

    close broadcast channel on first event and unmount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import * as React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
@@ -12,17 +13,25 @@ export default function App({ Component, pageProps }: AppProps) {
    * Register Broadcast Channel
    */
   const router = useRouter();
-  const bc = new BroadcastChannel(process.env.NEXT_PUBLIC_BC_CHANNEL as string);
-  bc.onmessage = async (event: MessageEvent<string>) => {
-    if (event.data === 'sign-out') {
-      try {
-        await (await fetch('/api/sign-out')).json();
-        router.push('/');
-      } catch (_err) {
-        router.push('/');
+  React.useEffect(() => {
+    let bc: BroadcastChannel;
+    bc = new BroadcastChannel(process.env.NEXT_PUBLIC_BC_CHANNEL as string);
+    bc.onmessage = async (event: MessageEvent<string>) => {
+      if (event.data === 'sign-out') {
+        bc.close();
+        try {
+          await (await fetch('/api/sign-out')).json();
+          router.push('/');
+        } catch (_err) {
+          router.push('/');
+        }
       }
-    }
-  };
+    };
+    return () => {
+      bc.close();
+    };
+  }, []);
+
   return (
     <main className={inter.className}>
       <Head>
