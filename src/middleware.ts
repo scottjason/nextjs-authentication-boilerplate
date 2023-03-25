9d9d9d455dd579336e973f15d192557e399865f6
scottjason <scottleejason@gmail.com>
2023-03-25 00:31:54 -0700

    update middleware, re-route 404s

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,18 @@ import { NextRequest, NextResponse } from 'next/server';
 import { authenticate } from './utils/middleware/auth';
 
 export async function middleware(req: NextRequest, res: NextResponse) {
-  return authenticate(req, res);
+  const {
+    nextUrl: { origin, pathname },
+  } = req;
+
+  const validPaths = ['/', '/dashboard'];
+  if (!validPaths.includes(pathname)) {
+    return NextResponse.redirect(origin);
+  } else {
+    return authenticate(req, res);
+  }
 }
 
 export const config = {
-  matcher: ['/', '/dashboard'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
